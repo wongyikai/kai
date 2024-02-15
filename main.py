ae88b161c9eb91ece128226433a2eb7257995432
wongyikai <157562975+wongyikai@users.noreply.github.com>
2024-02-15 15:36:58 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""CircuitPython Essentials: PWM with Fixed Frequency example."""
+import time
+import board
+import pwmio
+
+# LED setup for most CircuitPython boards:
+led = pwmio.PWMOut(board.LED, frequency=50000, duty_cycle=0, variable_frequency = True)
+# LED setup for QT Py M0:
+# led = pwmio.PWMOut(board.SCK, frequency=5000, duty_cycle=0)
+
+
+while True:
+    for i in range(100):
+        # PWM LED up and down
+        if i < 50:
+            led.duty_cycle = int(i * 2 * 65535 / 100)  # Up
+        else:
+            led.duty_cycle = 65535 - int((i - 50) * 2 * 65535 / 100)  # Down
+        time.sleep(0.01)
+
